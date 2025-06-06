83a87efeca2d9b9a28687593b773813695f81765
liamding <liangding.liam@gmail.com>
2025-06-06 19:17:42 +0800

    grpo stage-2

--- eval_fast.sh
+++ eval_fast.sh
@@ -1,14 +1,14 @@
 export VLLM_WORKER_MULTIPROC_METHOD=spawn # Required for vLLM
-export CUDA_VISIBLE_DEVICES=0,1,2,3 # Use GPU 2 and 3
-export NUMEXPR_MAX_THREADS=128 # Utilize all 128 cores for numerical computations
+export CUDA_VISIBLE_DEVICES=1
+export NUMEXPR_MAX_THREADS=128 # Utilize 128 cores for numerical computations
 
-NUM_GPUS=4
+NUM_GPUS=1
 
 #MODEL=data/OpenR1-Distill-0.6B
 #MODEL=Qwen/Qwen3-0.6B-Base
 MODEL=data/R1-Distill-0.6B-Qwen-GRPO
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:8192,temperature:0.6,top_p:0.95}"
-OUTPUT_DIR=data/evals/$MODEL
+OUTPUT_DIR=data/evals/${MODEL}_stage1
 
 #MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 
@@ -19,10 +19,10 @@ OUTPUT_DIR=data/evals/$MODEL
 #    --output-dir $OUTPUT_DIR
 
 # Math 500  
-#TASK=math_500
-#lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#    --use-chat-template \
-#    --output-dir $OUTPUT_DIR
+TASK=math_500
+lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 # GPQA Diamond
 TASK=gpqa:diamond
